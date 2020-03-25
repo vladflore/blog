6ea971060b7c44125be4775aa573b519b2ad7e40
Vlad Flore <flore.vlad@gmail.com>
2020-03-25 14:36:16 +0100

    added script to publish blog automatically

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 rm -rf _site/
 jekyll build
 rm -rf docs/
@@ -7,4 +9,4 @@ cp _site/*.xml docs/
 cp -r _site/assets docs/
 git add docs/
 git commit -m "published new blog version"
-git push origin master
+git push git@github.com:vladflore/blog.git master
