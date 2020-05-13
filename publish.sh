7267e4e89506d6cafb3df424131f4be7d4653857
Vlad Flore <flore.vlad@gmail.com>
2020-05-13 10:37:15 +0200

    change blog publishing script and config

--- publish.sh
+++ publish.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-echo delete old _site/
-rm -rf _site/
-echo build site
+rm -rf _site
+rm -rf docs
+mkdir docs
 jekyll build
-echo reset docs/
-rm -rf docs/ && mkdir docs/
-#echo copy site files to docs/ for publishing
-#cp _site/*.html docs/
-#cp _site/*.xml docs/
-#cp -r _site/assets docs/
-#echo git add docs/
-#git add docs/
-#echo git commit new blog version
-#git commit -m "published new blog version"
-#echo git push to GitHub
-#git push git@github.com:vladflore/blog.git master
+cp -r _site/** docs
+git add docs
+git commit -m "published new blog version"
+git push git@github.com:vladflore/blog.git master
