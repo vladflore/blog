444f0e44a17f174b435fe1196ff748025dc89070
Vlad Flore <flore.vlad@gmail.com>
2020-07-04 12:11:00 +0200

    change publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-rm -rf _site
-rm -rf docs
-mkdir docs
+
+WORKING_DIR=_site
+PUBLISH_DIR=docs
+if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
+if [ -d "$PUBLISH_DIR" ]; then
+  rm -Rf $PUBLISH_DIR
+  mkdir $PUBLISH_DIR
+fi
 JEKYLL_ENV=production jekyll build
-cp -r _site/** docs
-git add docs
+cp -r $WORKING_DIR/** $PUBLISH_DIR
+git add $PUBLISH_DIR
 git commit -m "published new blog version"
 git push git@github.com:vladflore/blog.git master
