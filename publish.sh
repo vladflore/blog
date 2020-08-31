48c15cad314502b71542698107947e9b63559c73
Vlad Flore <flore.vlad@gmail.com>
2020-08-31 23:36:26 +0200

    update publish script

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ PUBLISH_DIR=docs
 if [ -d "$WORKING_DIR" ]; then rm -Rf $WORKING_DIR; fi
 if [ -d "$PUBLISH_DIR" ]; then rm -Rf $PUBLISH_DIR; fi
 mkdir $PUBLISH_DIR
-JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 cp -r $WORKING_DIR/** $PUBLISH_DIR
 git add $PUBLISH_DIR
 git commit -m "published new blog version"
